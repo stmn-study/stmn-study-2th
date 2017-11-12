01f21f9a9e27dad4483bf6a07d22d6bb648ec5ae
Yoshimatsuo <matsukawa.y.ac@gmail.com>
2017-11-12 18:22:45 +0900

    コメント追加

--- lib/utils/youtube_data_api.rb
+++ lib/utils/youtube_data_api.rb
@@ -20,6 +20,46 @@ class YoutubeDataApi
     end
 
     # Keyword_Search
+    # ex) response_sample
+    #
+    #  {
+    #   "kind": "youtube#searchListResponse",
+    #   "etag": "\"IHLB7Mi__JPvvG2zLQWAg8l36UU/B7HTIgYFvqFP1W51kJxRs_VPTEs\"",
+    #   "pageInfo": {
+    #   "totalResults": 2,
+    #   "resultsPerPage": 5
+    #  },
+    #   "items": [
+    #   {
+    #     "kind": "youtube#searchResult",
+    #     "etag": "\"IHLB7Mi__JPvvG2zLQWAg8l36UU/9Q6jCT_HeHFfR4LxWgkefRtExOk\"",
+    #     "id": {
+    #       "kind": "youtube#video",
+    #       "videoId": "J9VsVp7n6mg"
+    #     },
+    #     "snippet": {
+    #       "publishedAt": "2009-08-05T10:45:24.000Z",
+    #       "channelId": "UCnUSZE-pn6_g2DQu_d78fyw",
+    #       "title": "ミニチュア風動画 - 福岡市中央区赤坂",
+    #       "description": "http://akihiro.jugem.jp/?eid=259.",
+    #       "thumbnails": {
+    #         "default": {
+    #           "url": "https://i.ytimg.com/vi/J9VsVp7n6mg/default.jpg"
+    #         },
+    #         "medium": {
+    #           "url": "https://i.ytimg.com/vi/J9VsVp7n6mg/mqdefault.jpg"
+    #         },
+    #         "high": {
+    #           "url": "https://i.ytimg.com/vi/J9VsVp7n6mg/hqdefault.jpg"
+    #         }
+    #       },
+    #       "channelTitle": "1977akihiro",
+    #       "liveBroadcastContent": "none"
+    #     }
+    #    }
+    #   }
+    #
+
     def search_param_keys
       [
         :part,
@@ -52,7 +92,12 @@ class YoutubeDataApi
 
         search_response.data.items.each do |search_result|
           case search_result.id.kind
-
+            when 'youtube#video'
+              videos << "#{search_result.snippet.title} (#{search_result.id.videoId})"
+            when 'youtube#channel'
+              channels << "#{search_result.snippet.title} (#{search_result.id.channelId})"
+            when 'youtube#playlist'
+              playlists << "#{search_result.snippet.title} (#{search_result.id.playlistId})"
           end
         end
 
