008bd97b2fc6633f9b1701d5d23ab4f1c42f7bed
Yoshimatsuo <matsukawa.y.ac@gmail.com>
2017-11-17 20:01:48 +0900

    もろもろ完成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    params[:video_id] ||= "4JkIs37a2JE"
+    @video = params[:video_id]
+
     search_params = {}
     search_params.merge!(q:params[:q], max_results:25)
 
