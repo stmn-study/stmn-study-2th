be4f2ea95475f3ad1fe333ada728e80482441fc2
shule517 <shule517@gmail.com>
2017-11-24 23:09:20 +0900

    動画情報収集処理をリファクタリング

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,6 @@ class HomeController < ApplicationController
   def index
     params[:video_id] ||= "4JkIs37a2JE"
     @video = params[:video_id]
-
-    search_params = {}
-    search_params.merge!(q:params[:q], max_results:25)
-
-    if search_params.present?
-      @results = YoutubeDataApi.search(search_params)
-    end
+    @results = YoutubeDataApi.search(q: params[:q], max_results: 25)
   end
 end
