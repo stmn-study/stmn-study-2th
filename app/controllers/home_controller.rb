05a7dd58dfe809282ee9d1359fb572ca93754197
shule517 <shule517@gmail.com>
2017-11-25 00:09:05 +0900

    初回時はTUNAGの紹介動画を表示。検索した場合は、１件目の動画を表示。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
   def index
-    params[:video_id] ||= "4JkIs37a2JE"
-    @video_id = params[:video_id]
+    params[:q] ||= '組織エンゲージメントクラウド TUNAG'
     @results = YoutubeDataApi.search(q: params[:q], max_results: 25)
+    @video_id = @results.first[:video_id]
+
+    if params[:video_id]
+      @video_id = params[:video_id]
+    end
   end
 end
