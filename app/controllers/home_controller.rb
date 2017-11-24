745b2fa2ee5210bcbebf4063db38519429dd8eaf
shule517 <shule517@gmail.com>
2017-11-24 23:13:38 +0900

    video -> video_id

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     params[:video_id] ||= "4JkIs37a2JE"
-    @video = params[:video_id]
+    @video_id = params[:video_id]
     @results = YoutubeDataApi.search(q: params[:q], max_results: 25)
   end
 end
